e872d660c4c05c673825a5ac1be4342fcf4c6a4d
TeacherKing47 <32286815+jjimmyk@users.noreply.github.com>
2021-06-22 15:54:08 -0700

    Update index.js

--- basic/scripts/index.js
+++ basic/scripts/index.js
@@ -1,1 +1,27 @@
+const questionObject = {
+  question: 'What is my question?',
+  answer: 'This is my answer',
+  displayQuestion: function () {
+ 
+  alert( this.question );
+ 
+  }
+}
+class question {
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+    this.correct = false;
+  }
+}
+let quiz = [];
 
+quiz.push(new question('What is 3+5?', '8'));
+quiz.push(new question('What is your favorite color?', 'blue, I mean red'));
+quiz.push(new question('What is optimal swallow velocity?', 'I do not know!'));
+
+let i = 0;
+while (i < quiz.length) {
+  console.log(quiz[i].question + " " + quiz[i].answer);
+  i++;
+}
