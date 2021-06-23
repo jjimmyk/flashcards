7b773d46b0c4e09c83db63fc399344d085e65172
TeacherKing47 <32286815+jjimmyk@users.noreply.github.com>
2021-06-22 21:03:26 -0700

    Update index.js

--- basic/scripts/index.js
+++ basic/scripts/index.js
@@ -1,4 +1,5 @@
 
+//Define the class of questions.
 class question {
   constructor(question, answer) {
     this.question = question;
@@ -6,15 +7,24 @@ class question {
     this.correct = false;
   }
 }
+  
+//Create the array of quiz flashcards. 
 let quiz = [];
 
+//Add the three objects to the array.
 quiz.push(new question('What is 3+5?', '8'));
 quiz.push(new question('What is your favorite color?', 'blue, I mean red'));
 quiz.push(new question('What is optimal swallow velocity?', 'I do not know!'));
 
+
+//Wait for the user to click on the button. When user clicks button, set counter to 0. Then, while counter is less than length of the array, print the answer of the
+//item in the array.
+document.getElementById("What is 3+5?").addEventListener("click", function() {
+
 let i = 0;
 while (i < quiz.length) {
-  console.log(quiz[i].question + " " + quiz[i].answer);
+  console.log(quiz[i].answer);
   i++;
 }
 
+}
